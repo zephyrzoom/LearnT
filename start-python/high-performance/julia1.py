c948a716c85bdc75623141762335f2f10c4af535
707 <707472783@qq.com>
2018-07-07 17:58:00 +0800

    更新xmind

--- start-python/high-performance/julia1.py
+++ start-python/high-performance/julia1.py
@@ -5,7 +5,6 @@ import time
 x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
 c_real, c_imag = -0.62772, -.42193
 
-
 def calc_pure_python(desired_width, max_iterations):
     """Create a list of complex coordinates (zs) and complex
     parameters (cs), build Julia set, and display"""
