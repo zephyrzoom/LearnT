6405231deb1ccadf199b6d1715cd38abd17123e5
707 <707472783@qq.com>
2019-02-16 21:19:58 +0800

    update guessing game

--- rust/guessing_game/target/debug/guessing_game.d
+++ rust/guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/Users/zzq7191/Documents/LearnY/rust/guessing_game/target/debug/guessing_game.dSYM: /Users/zzq7191/Documents/LearnY/rust/guessing_game/src/main.rs
+/home/zeek/Documents/LearnY/rust/guessing_game/target/debug/guessing_game: /home/zeek/Documents/LearnY/rust/guessing_game/src/main.rs
