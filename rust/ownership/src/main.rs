1935d312bf9abfdc7329cfcbea680ac5833ada7c
707 <707472783@qq.com>
2019-03-23 18:55:06 +0800

    添加ownership和reference_borrow

---
+++ rust/ownership/src/main.rs
@@ -0,0 +1,69 @@
+fn main() {
+    let s = "hello";
+
+    let s = String::from("hello");
+
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
+
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    // println!("{}, world!", s1);
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    let s = String::from("hello");  // s comes into scope
+    takes_ownership(s);             // s's value moves into the function...
+    // println!("{}", s);
+    let x = 5;                      // x comes into scope
+    makes_copy(x);
+    println!("{}", x);
+
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
+
+    let some_string = String::from("hello"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
+
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
+}
---
+++ rust/reference_borrow/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("s = {}", s);
+
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+    }
+    let r2 = &mut s;
+    println!("{}, {}", r2, r2);
+
+    let mut s = String::from("hello");
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+    // println!("{}, {}, and {}", r1, r2, r3);
+
+    let reference_to_nothing = dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+    // &s
+    s
+}
