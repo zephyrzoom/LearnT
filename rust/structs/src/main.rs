e403389925a5c44ecf3a4d0a01c24219c42f72f2
707 <707472783@qq.com>
2019-03-31 14:02:07 +0800

    添加计算面积的工程

--- rust/structs/src/main.rs
+++ rust/structs/src/main.rs
@@ -19,6 +19,9 @@ fn main() {
         username: String::from("anotherusername567"),
         ..user1
     };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
 
 struct User {
@@ -36,3 +39,13 @@ fn build_user(email: String, username: String) -> User {
         sign_in_count: 1,
     }
 }
+
+// struct User {
+//     username: &str,
+//     email: &str,
+//     sign_in_count: u64,
+//     active: bool,
+// }
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
