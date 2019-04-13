ba3dd0f39a12dcf2d873c5a5eedf2d62dcdf30ea
707 <707472783@qq.com>
2019-04-13 13:36:21 +0800

    增加struct method部分

--- rust/struct_calc_area/src/main.rs
+++ rust/struct_calc_area/src/main.rs
@@ -15,10 +15,23 @@ fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        // area(&rect1)
+        rect1.area()
     );
 
     println!("rect1 is {:#?}", rect1);
+
+//////////////////////////////////////////////////
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+//////////////////////////////////////////////////////////
+
+    let sq = Rectangle::square(3);
 }
 
 // fn area(width: u32, height: u32) -> u32 {
@@ -37,4 +50,18 @@ fn area(rectangle: &Rectangle) -> u32 {
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
