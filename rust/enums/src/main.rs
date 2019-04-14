508c7212560f7e44b94667207341da942bb2b182
707 <707472783@qq.com>
2019-04-14 18:53:14 +0800

    更新enum

---
+++ rust/enums/src/main.rs
@@ -0,0 +1,81 @@
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    // let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    // let loopback = IpAddr::V6(String::from("::1"));
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    // 不同类型会报错
+    // let sum = x + y;
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(ip_type:IpAddrKind) {}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
