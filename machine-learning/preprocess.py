f96e889033d49124729ca75af27aab7cb7b70f24
707 <707472783@qq.com>
2019-10-02 13:37:33 +0800

    给代码写注释

--- machine-learning/preprocess.py
+++ machine-learning/preprocess.py
@@ -1,22 +1,26 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.impute import SimpleImputer
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,-1].values
 print(dataset)
 
-imputer = Imputer()
+imputer = SimpleImputer()
 X[:,1:3] = imputer.fit_transform(X[:,1:3])
 print(X)
-labelencoder_X = LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
+X.fillna(X.mode(), inplace=True)
 print(X)
+# labelencoder_X = LabelEncoder()
+# X[:,0] = labelencoder_X.fit_transform(X[:,0])
+# print(X)
 
-onehotencoder  = OneHotEncoder(categorical_features=[0])
-X = onehotencoder.fit_transform(X).toarray()
-labelencoder_Y = LabelEncoder()
-Y = labelencoder_Y.fit_transform(Y)
-print(X)
-print(Y)
+
+# onehotencoder  = OneHotEncoder(categorical_features=[0])
+# X = onehotencoder.fit_transform(X).toarray()
+# labelencoder_Y = LabelEncoder()
+# Y = labelencoder_Y.fit_transform(Y)
+# print(X)
+# print(Y)
--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,36 +1,49 @@
 use std::io;
-use rand::Rng;
+use rand::Rng;  // Rng为trait，定义了随机数生成器
 use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
+    // thread_rng获取当前线程和随机数种子，gen_range是Rng引入的方法，生成1-100随机数
+    // 不知道引入的库如何使用，可以通过cargo doc --open查看文档
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     // println!("The secret number is: {}", secret_number);
 
 
+    // 无限循环
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
-
+        // stdin返回io::Stdin终端输入对象
+        // read_line获取用户输入
+        // 用户输入会作为字符串存储在guess，可修改字符串值，所以是mut
+        // &表示引用，传进来参数还是会指向外面的变量，用同一块存储空间
+        // read_line返回io::Result枚举类型，有Ok和Err两个值，Err时会包含错误信息，使用Result的expect方法来处理错误
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
+        // shadow前面的guess变量，不需要重新建变量了
+        // trim去掉前后空格，parse将字符串转为数字，指定类型为u32
+        // parse返回的也是Result类型，可以通过match语句来匹配
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+            Ok(num) => num, // 转换成功则返回num
+            Err(_) => continue, // 其他情况则会继续下一轮循环
         };
 
+        // {}是占位符，用后面的参数替换
         println!("You guessed: {}", guess);
 
+        // Ordering也是枚举类型
+        // match类似于switch语句，匹配上了就停止，不用加break
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
-                break;
+                break;  // 跳出循环
             }
         }
     }
