3bc4ecc9ea16be56640c17d6cced8a6bcca06e01
707 <707472783@qq.com>
2019-11-03 21:47:52 +0800

    slice和struct

--- rust/slice/src/main.rs
+++ rust/slice/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // 切片数据没有所有权，可以建立切片的引用
     let mut s = String::from("hello world");
     let word = first_word(&s); // word will get the value 5
     println!("the first word is: {}", word);
@@ -6,7 +7,7 @@ fn main() {
     // println!("the first word is: {}", word);
 
     let s = String::from("hello world");
-    let hello = &s[0..5];
+    let hello = &s[0..5];   // 引用字符串的一部分，0,1,2,3,4,
     let world = &s[6..11];
 
     let s = String::from("hello world");
@@ -15,17 +16,17 @@ fn main() {
 
     let s = String::from("hello");
     let slice = &s[0..2];
-    let slice = &s[..2];
+    let slice = &s[..2];    // 和上面的相等
 
     let s = String::from("hello");
     let len = s.len();
     let slice = &s[3..len];
-    let slice = &s[3..];
+    let slice = &s[3..];    // 和上面的相等
 
     let s = String::from("hello");
     let len = s.len();
     let slice = &s[0..len];
-    let slice = &s[..];
+    let slice = &s[..];     // 和上面的相等
 
     let s = "Hello, world!";
 
@@ -43,10 +44,11 @@ fn main() {
     let slice = &a[1..3];
 }
 
+// 没有获取字符串所有权，只能通过返回结束下标达到目的，不过这种方式是有问题的，返回的位置数据可能由于字符串的改变而失效
 // fn first_word(s: &String) -> usize {
-//     let bytes = s.as_bytes();
-
-//     for (i, &item) in bytes.iter().enumerate() {
+//     let bytes = s.as_bytes();    // 将字符串转换为字节数组匹配
+       // iter返回数组的每个元素，enumerate包装每个元素为元组，元组格式（序号，元素引用）
+//     for (i, &item) in bytes.iter().enumerate() { 
 //         if item == b' ' {
 //             return i;
 //         }
@@ -55,6 +57,7 @@ fn main() {
 //     s.len()
 // }
 
+// 这种写法的好处是会让s.clear报错，因为有个不易变引用了，再用易变引用修改值就是不允许的
 // fn first_word(s: &String) -> &str {
 //     let bytes = s.as_bytes();
 
--- rust/structs/src/main.rs
+++ rust/structs/src/main.rs
@@ -1,29 +1,37 @@
+// 结构体可以比作有名字的元组，所以可以不按顺序，随机访问
 fn main() {
+
+    // 定义实例方法如下，结构体相当于模版，定义实例来赋值
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
+    // 如果是易变的，可以用点访问和赋值，易变一定是真的整个实例，不能是某个字段
     user1.email = String::from("anotheremail@example.com");
 
     let user2 = User {
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
-        active: user1.active,
+        active: user1.active,   // 使用其他实例点字段赋值
         sign_in_count: user1.sign_in_count,
     };
 
     let user2 = User {
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
-        ..user1
+        ..user1 // 缩写方法，其他字段使用user1的字段值
     };
 
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
+    black.0 // 可以通过下标访问
+
+    // 还可以创建无字段的结构体
 }
 
+// 定义方法就是这样
 struct User {
     username: String,
     email: String,
@@ -31,15 +39,17 @@ struct User {
     active: bool,
 }
 
+// 可以在方法中定义实例隐式返回
 fn build_user(email: String, username: String) -> User {
     User {
-        email,
+        email,  // 使用和参数同名点变量，可以省略，相当于email:email
         username,
         active: true,
         sign_in_count: 1,
     }
 }
 
+// 目前还无法使用引用类型定义结构体字段，需要配合生命周期，后面才讲
 // struct User {
 //     username: &str,
 //     email: &str,
@@ -47,5 +57,6 @@ fn build_user(email: String, username: String) -> User {
 //     active: bool,
 // }
 
+// 结构体元组，可以用来区分类型相同的元组
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
