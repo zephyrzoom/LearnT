8268498a580ece00c400a0e37e03e04929876696
707 <707472783@qq.com>
2020-02-01 20:53:00 +0800

    update minigrep

---
+++ rust/minigrep/src/lib.rs
@@ -0,0 +1,114 @@
+use std::fs;
+use std::error::Error;
+use std::env;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        // 用Result可以让main函数知道创建的情况
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err(); // 如果CASE_INSENSITIVE设置了，就会返回false
+        Ok(Config { query, filename, case_sensitive })
+    }
+}
+
+// pub fn run(config: Config) -> Result<(), Box<dyn Error>> {  // dyn意思是dynamic，可以返回实现Error trait的各种类型
+//     let content = fs::read_to_string(config.filename)?;
+//     println!("With text:\n{}", content);
+//     Ok(())
+// }
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {  // dyn意思是dynamic，可以返回实现Error trait的各种类型
+    let contents = fs::read_to_string(config.filename)?;
+    // for line in search(&config.query, &content) {
+    //     println!("{}", line);
+    // }
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+// 需要给出生命周期
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     vec![]
+// }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();   // 该方法创建了新字符串，而不是之前的切片引用
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
---
+++ rust/minigrep/src/main.rs
@@ -0,0 +1,92 @@
+/**
+ * 程序划分的准则是3条：
+ * 1. 把代码分为lib.rs和main.rs，代码逻辑放在lib.rs
+ * 2. 命令行处理简单的放在main.rs
+ * 3. 命令行处理复杂的放在lib.rs
+ */
+use std::env; // 非unicode使用std::env::args_os
+// use std::fs;
+use std::process;
+// use std::error::Error;
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect(); // 接收命令行参数
+
+    // let query = &args[1];
+    // let filename = &args[2];
+
+    // let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 闭包，就是匿名函数
+        eprintln!("Problem parsing arguments: {}", err);    // 标准错误打印，错误信息会打印在终端，正确信息才会在文件中
+        process::exit(1); // 终止程序并返回错误码
+    });
+
+    // println!("Searching for {}", config.query);
+    // println!("in file {}", config.filename);
+
+    // let content = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    // println!("With text:\n{}", content);
+
+    // run(config);
+    if let Err(e) = minigrep::run(config) {   // 正确时返回空，所以不需要unwrap正确返回，只需处理错误信息
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+// fn parse_config(args: &[String]) -> (&str, &str) {
+//     let query = &args[1];
+//     let filename = &args[2];
+//     (query, filename)
+// }
+
+// 使用结构体
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();    // 克隆会占用大量时间空间，尽量避免使用，这里为了简便所以使用
+//     let filename = args[2].clone();
+//     Config {query, filename}
+// }
+
+// struct Config {
+//     query: String,
+//     filename: String,
+// }
+
+// impl Config {
+//     fn new(args: &[String]) -> Config {
+//         if args.len() < 3 {
+//             panic!("not enough arguments");
+//         }
+//         let query = args[1].clone();
+//         let filename = args[2].clone();
+//         Config {query, filename}
+//     }
+// }
+
+// impl Config {
+//     fn new(args: &[String]) -> Result<Config, &'static str> {
+//         // 用Result可以让main函数知道创建的情况
+//         if args.len() < 3 {
+//             return Err("not enough arguments");
+//         }
+//         let query = args[1].clone();
+//         let filename = args[2].clone();
+//         Ok(Config { query, filename })
+//     }
+// }
+
+// 把逻辑提取出来
+// fn run(config: Config) {
+//     let content =
+//         fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+//     println!("With text:\n{}", content);
+// }
+
+// 返回错误信息
+// fn run(config: Config) -> Result<(), Box<dyn Error>> {  // dyn意思是dynamic，可以返回实现Error trait的各种类型
+//     let content = fs::read_to_string(config.filename)?;
+//     println!("With text:\n{}", content);
+//     Ok(())
+// }
